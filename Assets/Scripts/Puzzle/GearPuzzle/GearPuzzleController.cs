9d79daf0e138ed73e5b0ba80ef2ddf491ba7680a
FME849 <1414439@gmail.com>
2026-07-03 22:41:06 +0700

    add GearPuzzleController stub

---
+++ Assets/Scripts/Puzzle/GearPuzzle/GearPuzzleController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Puzzle.GearPuzzle
+{
+    public class GearPuzzleController : PuzzleController
+    {
+        private void Update()
+        {
+            if (Keyboard.current.escapeKey.wasPressedThisFrame)
+                Complete(PuzzleResult.Cancelled);
+        }
+
+        // Call this when gear puzzle logic determines the puzzle is solved
+        public void OnPuzzleSolved() => Complete(PuzzleResult.Success);
+
+        // Call this when puzzle reaches a fail condition (e.g. time up)
+        public void OnPuzzleFailed() => Complete(PuzzleResult.Fail);
+    }
+}
